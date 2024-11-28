0cf7002ced204c3f8104cf157872519e96b05c3d
FedericoMassidda <chiccomassidda@gmail.com>
2024-11-28 16:31:51 +0100

    ho compresso tutto in una funzione

--- PhpExtra/index.php
+++ PhpExtra/index.php
@@ -77,5 +77,14 @@ function validatePassword(string $password) :void{
         echo "La password inserita NON rispetta i nostri criteri, controlla che:\n- Sia presente almeno una lettera maiuscola;\n- Sia presente almeno un carattere numerico;\n- La lunghezza sia almeno di 8 caratteri.";
     }
 }
-$password= 'cCcccccc1';
-validatePassword($password);
+$password= 'A123456b';
+// validatePassword($password);
+
+function test(string $password){
+    if(preg_match('/[A-Z]/', $password) && preg_match('/\d/', $password) && strlen($password) >= 8){
+        echo 'La password inserita rispetta i nostri criteri.';
+    }else{
+        echo "La password inserita NON rispetta i nostri criteri, controlla che:\n- Sia presente almeno una lettera maiuscola;\n- Sia presente almeno un carattere numerico;\n- La lunghezza sia almeno di 8 caratteri.";
+    }
+}
+test($password);

cf3c457ad01ae1ff0ccd7ac0ec77693a0e97edf2
FedericoMassidda <chiccomassidda@gmail.com>
2024-11-29 21:33:05 +0100

    ottimizzato il codice

--- PhpExtra/index.php
+++ PhpExtra/index.php
@@ -5,17 +5,13 @@
 // lunghezza di 8+ caratteri.
 
 
-$password= 'A123456b';
+$password= 'aaaA1bbb';
 
 function test(string $password){
-    if(preg_match('/[A-Z]/', $password) && preg_match('/\d/', $password) && strlen($password) >= 8){
+    if(preg_match('/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?!.*\s)(?=.{8,})/', $password)){
         echo 'La password inserita rispetta i nostri criteri.';
     }else{
         echo "La password inserita NON rispetta i nostri criteri, controlla che:\n- Sia presente almeno una lettera maiuscola;\n- Sia presente almeno un carattere numerico;\n- La lunghezza sia almeno di 8 caratteri.";
     }
 }
-
-// test($password);
-
-// Stessa funzione però con l'operatore ternario
-echo preg_match('/[A-Z]/', $password) && preg_match('/\d/', $password) && strlen($password) >= 8 ? 'La password inserita rispetta i nostri criteri.' : "La password inserita NON rispetta i nostri criteri, controlla che:\n- Sia presente almeno una lettera maiuscola;\n- Sia presente almeno un carattere numerico;\n- La lunghezza sia almeno di 8 caratteri.";
+test($password);
